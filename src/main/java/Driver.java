6abfac1325a8269d9880c72fbf6c3790375a3d61
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-02 08:22:51 -0700

    page rank finished

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -25,55 +25,23 @@ public class Driver {
         Path prFilePath = new Path(args[1]);
         Path outputPath = new Path(args[2]);
         float beta = Float.parseFloat(args[3]);
+        UnitMultiplication multiplication = new UnitMultiplication();
+        UnitSum sum = new UnitSum();
 
-        // mapper1 reducer1
-        Configuration configuration1 = new Configuration();
-        configuration1.setFloat("beta", beta);
-        Job job1 = Job.getInstance(configuration1);
-        job1.setJarByClass(UnitMultiplication.class);
-
-        File outPutFile1 = new File(args[2]);
-        if (outPutFile1.exists()) {
-            System.out.println("Output1 directory already exits!\nDelete previous output directory.");
-            FileUtils.deleteDirectory(outPutFile1);
+        // example for calling two main methond two class separately
+        //args0: dir of transition.txt
+        //args1: dir of PageRank.txt
+        //args2: dir of unitMultiplication result
+        //args3: times of convergence
+        String transitionMatrix = args[0];
+        String prMatrix = args[1];
+        String unitState = args[2];
+        int count = Integer.parseInt(args[3]);
+        for(int i=0;  i<count;  i++) {
+            String[] args1 = {transitionMatrix, prMatrix+i, unitState+i};
+            multiplication.main(args1);
+            String[] args2 = {unitState + i, prMatrix+(i+1)};
+            sum.main(args2);
         }
-
-        ChainMapper.addMapper(job1, UnitMultiplication.TransitionMapper.class, Object.class, Text.class, Text.class, Text.class, configuration1);
-        ChainMapper.addMapper(job1, UnitMultiplication.PRMapper.class, Object.class, Text.class, Text.class, Text.class, configuration1);
-
-        job1.setReducerClass(UnitMultiplication.MultiplicationReducer.class);
-
-        job1.setOutputKeyClass(Text.class);
-        job1.setOutputValueClass(Text.class);
-
-        MultipleInputs.addInputPath(job1, transitionFilePath, TextInputFormat.class, UnitMultiplication.TransitionMapper.class);
-        MultipleInputs.addInputPath(job1, prFilePath, TextInputFormat.class, UnitMultiplication.PRMapper.class);
-
-        FileOutputFormat.setOutputPath(job1, outputPath);
-        job1.waitForCompletion(true);
-
-        // second task
-        Path unitInputPath = new Path(args[4]);
-        Path unitOutputPath= new Path(args[5]);
-
-        File outPutFile2 = new File(args[5]);
-        if (outPutFile2.exists()) {
-            System.out.println("Output2 directory already exists\nDelete previous output directory.");
-            FileUtils.deleteDirectory(outPutFile2);
-        }
-        // mapeper2 reducer2
-        Configuration configuration2 = new Configuration();
-        Job job2 = Job.getInstance(configuration1);
-        job2.setJarByClass(UnitSum.class);
-
-        job2.setMapperClass(UnitSum.SumMapper.class);
-        job2.setReducerClass(UnitSum.SumReducer.class);
-
-        job2.setOutputKeyClass(Text.class);
-        job2.setOutputValueClass(DoubleWritable.class);
-
-        FileInputFormat.addInputPath(job2, unitInputPath);
-        FileOutputFormat.setOutputPath(job2, unitOutputPath);
-        job2.waitForCompletion(true);
     }
 }
