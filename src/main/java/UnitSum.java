a67761111150710ebf378ed1a33cada671d67bc1
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-01 22:18:00 -0700

    add unit sum class

--- src/main/java/UnitSum.java
+++ src/main/java/UnitSum.java
@@ -1,6 +1,50 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * Created by zzc on 7/29/17.
  */
 public class UnitSum
 {
+    public static class SumMapper extends Mapper<Object, Text, Text, Text> {
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString().trim();
+            System.out.println(line);
+        }
+    }
+
+    public static class SumReducer extends Reducer<Object, Text, Text, Text> {
+        @Override
+        protected void reduce(Object key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            super.reduce(key, values, context);
+        }
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Path unitInputPath = new Path(args[0]);
+        Path unitoutputPath= new Path(args[1]);
+
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+        job.setJarByClass(UnitSum.class);
+
+        job.setMapperClass(SumMapper.class);
+        job.setReducerClass(SumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        FileInputFormat.addInputPath(job, unitInputPath);
+        FileOutputFormat.setOutputPath(job, unitoutputPath);
+        job.waitForCompletion(true);
+    }
 }
